cc6cf118d8442ab08306da5f548edb49cb80c658
slaterslater <slaterslater@gmail.com>
2021-09-20 21:49:21 -0400

    * installs no-sweat eslint, styled-components, keystonejs * removes pages/api * updates index.tsx to render sample data

---
+++ keystone.ts
@@ -0,0 +1,21 @@
+// keystone.ts
+
+import { config, list } from '@keystone-next/keystone';
+import { text } from '@keystone-next/keystone/fields';
+
+const Post = list({
+  fields: {
+    title: text({ isRequired: true }),
+    slug: text({ isIndexed: 'unique', isFilterable: true }),
+    content: text(),
+  },
+});
+
+export default config({
+  db: { provider: 'sqlite', url: 'file:./app.db' },
+  experimental: {
+    generateNextGraphqlAPI: true,
+    generateNodeAPI: true,
+  },
+  lists: { Post },
+});
